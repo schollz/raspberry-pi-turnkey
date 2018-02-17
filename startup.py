d28565f9b4da5cae79aa11eb0bf6d2b088ea838c
Zack Scholl <zack.scholl@gmail.com>
2018-02-17 07:30:16 -0800

    Add sleeps

--- startup.py
+++ startup.py
@@ -59,6 +59,7 @@ if __name__ == "__main__":
             f.write(json.dumps(s))    
         subprocess.Popen("./enable_ap.sh")
     elif s['status'] == 'connected':
+        ## ADD YOUR STARTUP COMMANDS HERE
         pass
     else:
         app.run(host="0.0.0.0",port=80)
