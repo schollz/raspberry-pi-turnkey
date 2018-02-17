9f4c875a80fdafcc0da0f3aa0ffab8df39e30d41
Zack Scholl <zack.scholl@gmail.com>
2018-02-17 08:27:21 -0800

    Add systemctl

--- disable_ap.sh
+++ disable_ap.sh
@@ -3,9 +3,13 @@
 sleep 3
 
 # disable the AP
+sudo systemctl stop dnsmasq && sudo systemctl stop hostapd
 sudo cp config/hostapd.disabled /etc/default/hostapd
 sudo cp config/dhcpcd.conf.disabled /etc/dhcpcd.conf
 sudo cp config/dnsmasq.conf.disabled /etc/dnsmasq.conf
+sudo systemctl daemon-reload
+sudo systemctl restart dhcpcd
+sudo systemctl start hostapd && sudo systemctl start dnsmasq
 
 # load wlan configuration
 sudo cp wpa.conf /etc/wpa_supplicant/wpa_supplicant.conf
